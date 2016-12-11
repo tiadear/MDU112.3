5fe3b242996efe5fd3521fb3743d6df01eb757ec
Tatiana Roga <you@example.com>
2016-12-11 15:16:58 +1000

    player can move around and rotate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerController : MonoBehaviour {
 
-	public float scaleX = 0.1f;
-	public float scaleY = 0.1f;
+	//public float scaleX = 0.1f;
+	//public float scaleY = 0.1f;
+
+	private float horizontal;
+	private float vertical;
 
 	private Rigidbody2D rb;
-	private Vector2 userInput;
+	//private Vector2 userInput;
 
-	//public float movementForce = 10f;
-	//public float movementSpeed = 2f;
-	//public float rotationSpeed = 50f;
+	//public float movementForce = 1f;
+	public float movementSpeed = 5f;
+	public float rotationSpeed = 100f;
 
 	void Start () {
 		// retrieve rigidbody for player
@@ -22,46 +25,34 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () {
 
-		// retrieve the axes
-		float horizontal = Input.GetAxis ("Horizontal");
-		float vertical = Input.GetAxis ("Vertical");
+		// get the axes
+		horizontal = Input.GetAxis ("Horizontal");
+		vertical = Input.GetAxis ("Vertical");
 
 		// store axes in the user input variable
-		userInput = new Vector2 (horizontal, vertical);
-
-		//transform.position = transform.position + new Vector3 (userInput.x * scaleX, userInput.y * scaleY, 0.0f);
-
-		// Method 3 - part a (rotation)
-		//Vector3 rotationDelta = new Vector3(0, 0, -horizontal * rotationSpeed * Time.deltaTime);
-		//transform.Rotate (rotationDelta);
+		//userInput = new Vector2 (horizontal, vertical);
 	}
 
 	void FixedUpdate() {
 
-		// all physics changes should occur here
-		// bare minimum code goes here
+		// move the ladybug
+		Vector3 positionOffset = transform.right * vertical * movementSpeed * Time.fixedDeltaTime;
+		rb.MovePosition (transform.position + positionOffset);
 
-		// Method 1 of moving by physics
-		//rb.AddForce(userInput * movementForce);
-
-		// Method 2 - teleport with collision checks
-		//Vector2 newPosition = transform.position;
-		//newPosition += userInput * movementSpeed * Time.fixedDeltaTime;
-		//rb.MovePosition (newPosition);
-		rb.MovePosition (transform.position + new Vector3 (userInput.x * scaleX, userInput.y * scaleY, 0));
-
-		// Method 3 - part b (movement)
-		//rb.AddForce(transform.up * userInput.y * movementForce);
+		Vector3 rotationDelta = new Vector3(0, 0, -horizontal * rotationSpeed * Time.deltaTime);
+		transform.Rotate (rotationDelta);
 
 	}
+		
+
+
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		Debug.Log("Collided with "+ collision.collider.name);
 
-		// check if this is an obstacle
+		// check if the collider is an obstacle
+		// if so destroy it
 		if (collision.collider.CompareTag (Tags.Obstacle)) {
-			// tag match
-			// destroy obstacle
 			GameObject.Destroy (collision.collider.gameObject);
 			Debug.Log ("Leaf destroyed!");
 		}
@@ -78,4 +69,5 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerExit2D (Collider2D collider) {
 		Debug.Log("Left trigger zone "+ collider.name);
 	}
+
 }
